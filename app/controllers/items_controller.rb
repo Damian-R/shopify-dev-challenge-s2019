d59c3ea41ea5b8a94edef3630d2fc60a7be3060d
Damian Reiter <damian.reiter@gmail.com>
2019-01-11 11:25:46 -0500

    update seeds

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,18 @@
 class ItemsController < ApplicationController
   def index
-    @items = params[:available] == 'true' ? Item.where('inventory_count > 0') : Item.all
+    items = params[:available] == 'true' ? Item.where('inventory_count > 0') : Item.all
 
     if params[:category]
-      @items = @items.where('category = ?', params[:category])
+      items = items.where('category = ?', params[:category])
     end
 
-    render json: { items: @items, count: @items.count }, status: :ok
+    render json: { items: items, count: items.count }, status: :ok
   end
 
   def show
     item_id = params[:id]
-    @item = ensure_item_exists(item_id)
-    render json: { item: @item }, status: :ok
+    item = ensure_item_exists(item_id)
+    render json: { item: item }, status: :ok
   end
 
   def purchase
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Item.create(title: 'product 1', price: 10.4, inventory_count: 4, category: 'cars')
-Item.create(title: 'product 2', price: 4.0, inventory_count: 0, category: 'cars')
+Item.create(title: 'product 1', price: 10.4, inventory_count: 8, category: 'cars')
+Item.create(title: 'product 2', price: 4.0, inventory_count: 2, category: 'cars')
 Item.create(title: 'product 3', price: 5.7, inventory_count: 0, category: 'cars')
-Item.create(title: 'product 4', price: 2.5, inventory_count: 6, category: 'cars')
-Item.create(title: 'product 5', price: 1.8, inventory_count: 5, category: 'toys')
+Item.create(title: 'product 4', price: 2.5, inventory_count: 9, category: 'cars')
+Item.create(title: 'product 5', price: 1.8, inventory_count: 7, category: 'toys')
 Item.create(title: 'product 6', price: 11.5, inventory_count: 0, category: 'toys')
